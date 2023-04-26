44a3b7d34245c025178d0e47e7eb30a298c52a67
Nizar Rahman <102470420+nizarrahman@users.noreply.github.com>
2023-04-26 18:41:38 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -17,11 +17,11 @@ global.APIKeys = {
 //language 
 global.language = indonesia //change indonesia to english if you don't understand the language used by the bot
 
-global.BOT_TOKEN = "YOUR_TELEGRAM_BOT_TOKEN" //create bot here https://t.me/BotFather and get the bot token
-global.BOT_NAME = "Alphabot-Mdོ" //your bot name
-global.OWNER_NAME = "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //your name
-global.OWNER_NUMBER = "62887435047326" //your telegram number
-global.OWNER = ["https://t.me/zeeoneofc", "https://t.me/zeeone_ofc"] // pastikan username sudah sesuai agar fitur khusus owner bisa di pakai
+global.BOT_TOKEN = "6023963075:AAFFkRMVWhIIkmoHiJpLgQGRKpivFbiWzhA" //create bot here https://t.me/BotFather and get the bot token
+global.BOT_NAME = "Nai Bot Store" //your bot name
+global.OWNER_NAME = "ᴹᴿ᭄ Nizar Rahman ×፝֟͜×" //your name
+global.OWNER_NUMBER = "6281386945391" //your telegram number
+global.OWNER = ["https://t.me/nizar901", "https://t.me/nizar901"] // pastikan username sudah sesuai agar fitur khusus owner bisa di pakai
 global.THUMBNAIL = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
-global.DONASI = "./image/donasi.jpg" // foto donasi di folder image
+global.DONASI = "./image/lol.jpg" // foto donasi di folder image
 global.lang = language //don't change
